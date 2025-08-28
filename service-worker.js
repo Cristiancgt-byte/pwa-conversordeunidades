ac641e6d81d7b8b194198d630451c1ef4240dfcf
Cristiancgt-byte <cristian.gambat@uniagustiniana.edu.co>
2025-08-28 16:11:01 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,11 @@
 const CACHE_NAME = "conversor-cache-v1";
 const urlsToCache = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./script.js",
-  "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "index.html",
+  "style.css",
+  "script.js",
+  "manifest.json",
+  "icons/icon-192.png",
+  "icons/icon-512.png"
 ];
 
 // Instalar el SW
